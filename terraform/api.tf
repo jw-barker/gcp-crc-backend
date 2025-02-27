31b026e5b35c3c5661685ade15a7279b4d91fc1f
James Barker <barker.jw@protonmail.com>
2025-02-28 10:30:15 +1100

    fix: added required api

--- terraform/api.tf
+++ terraform/api.tf
@@ -60,7 +60,8 @@ locals {
     "dataplex.googleapis.com",
     "datastore.googleapis.com",
     "firebaserules.googleapis.com",
-    "firestore.googleapis.com",
+    "firestore.googleapis.com"
+    "firestoredatabase.googleapis.com",
     "iam.googleapis.com",
     "iamcredentials.googleapis.com",
     "logging.googleapis.com",
